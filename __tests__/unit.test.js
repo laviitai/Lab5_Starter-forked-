dd73570f31bd3addc32103dc859d50b2eebe1d3b
laviitai <itai.lavi2003@gmail.com>
2026-05-01 16:28:56 -0700

    added tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,87 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+
+test('valid phone number with area code', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('valid phone number without area code', () => {
+  expect(isPhoneNumber('456-7890')).toBe(true);
+});
+
+test('invalid phone number missing dash', () => {
+  expect(isPhoneNumber('1234567890')).toBe(false);
+});
+
+test('invalid phone number letters', () => {
+  expect(isPhoneNumber('xyz-abc-defg')).toBe(false);
+});
+
+
+test('valid email basic', () => {
+  expect(isEmail('test@gmail.com')).toBe(true);
+});
+
+test('valid email short domain', () => {
+  expect(isEmail('hello@ucsd.edu')).toBe(true);
+});
+
+test('invalid email missing at symbol', () => {
+  expect(isEmail('testgmail.com')).toBe(false);
+});
+
+test('invalid email missing domain ending', () => {
+  expect(isEmail('test@gmail')).toBe(false);
+});
+
+
+test('valid strong password with numbers', () => {
+  expect(isStrongPassword('Password123')).toBe(true);
+});
+
+test('valid strong password with underscore', () => {
+  expect(isStrongPassword('A_good1')).toBe(true);
+});
+
+test('invalid password starts with number', () => {
+  expect(isStrongPassword('1password')).toBe(false);
+});
+
+test('invalid password too short', () => {
+  expect(isStrongPassword('XY1')).toBe(false);
+});
+
+
+test('valid date with one digit month and day', () => {
+  expect(isDate('1/2/2026')).toBe(true);
+});
+
+test('valid date with two digit month and day', () => {
+  expect(isDate('12/25/2026')).toBe(true);
+});
+
+test('invalid date with dashes', () => {
+  expect(isDate('12-25-2026')).toBe(false);
+});
+
+test('invalid date with two digit year', () => {
+  expect(isDate('12/25/26')).toBe(false);
+});
+
+
+test('valid 3 digit hex color with hash', () => {
+  expect(isHexColor('#fff')).toBe(true);
+});
+
+test('valid 6 digit hex color without hash', () => {
+  expect(isHexColor('A1B2C3')).toBe(true);
+});
+
+test('invalid hex color with invalid letter', () => {
+  expect(isHexColor('#ggg')).toBe(false);
+});
+
+test('invalid hex color wrong length', () => {
+  expect(isHexColor('#ffff')).toBe(false);
+});
